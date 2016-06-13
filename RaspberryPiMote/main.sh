c36bb7ffdd7ee1555e46ff31eb7f2759f0f1cbb3
Blake Jacquot <blakejacquot@gmail.com>
2016-06-13 19:31:03 +0000

    Add timing info and image reporting

--- RaspberryPiMote/main.sh
+++ RaspberryPiMote/main.sh
@@ -3,12 +3,14 @@
 # next one. You may need to change IP address of server. Run IFconfig on it
 # to check current one (if not set to static)
 
+sleep_time_sec=5
+image_number=1
+dir_key="/home/pi/.ssh/pi_key2"
+#dir_server="blakejacquot@192.168.1.7:/home/blakejacquot/Desktop/pics/"
+
 while true; do
     # Set variables
-    sleep_time_sec=1
     DATE=$(date +%Y%m%d%H%M%S)
-    dir_key="/home/pi/.ssh/pi_key2"
-    #dir_server="blakejacquot@192.168.1.7:/home/blakejacquot/Desktop/pics/"
     file_to_copy="image"$DATE".jpg"
     
     #Echo variables
@@ -27,11 +29,13 @@ while true; do
     #Take image
     echo "Taking an image"
     echo " "
-    raspistill -v -e jpg -o $file_to_copy -ISO 400 --vflip
+    raspistill -e jpg -o $file_to_copy -ISO 400 --vflip
     echo "Done taking image"
     echo " "
 
     #Copy image to server
+    echo $dir_server
+    #scp -vi /home/pi/.ssh/pi_key2 image$DATE.jpg $dir_server / image$DATE.jpg
     scp -vi /home/pi/.ssh/pi_key2 image$DATE.jpg blakejacquot@192.168.1.7:/home/blakejacquot/Desktop/pics/image$DATE.jpg  
     echo "Copying file to host PC"
     $cmd
@@ -39,9 +43,13 @@ while true; do
     #Remove file from host and sleep
     echo "Remove file from local machine"
     rm *.jpg
-    sleep $sleep_time_sec
 
+    echo "Completed image number = " $image_number
+    let image_number+=1
     echo " "
     echo "***********************************************"
     echo " "
+    echo "Paused for " $sleep_time_sec " seconds"
+    sleep $sleep_time_sec
+
 done
